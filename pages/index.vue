5a63c3ccd548905e1f408273a0618de2b9affba8
noe <noe@diffeasy.com>
2019-05-16 04:25:23 +0900

    :art: 見せれるレベルに調整

--- pages/index.vue
+++ pages/index.vue
@@ -46,6 +46,10 @@
       <div class="title">
         <p>NYAAAN</p>
       </div>
+      <div class="info">
+        <p>2019/5/16 開設</p>
+        <p>レスポンシブに(そのうち)対応予定！</p>
+      </div>
       <Fishes />
     </div>
   </div>
@@ -131,106 +135,11 @@ export default {
   color: #67678f;
   }
 }
-.fish-icon {
+.info {
   position: absolute;
-  color: rgb(156, 193, 236);
-}
-.fish1 {
-  top: 4%;
-  right: 90%;
-  animation:fuwafuwa 3s infinite linear alternate;
-  &:hover + .circle {
-    display: block;
-    top: 4%;
-    right: 90%;
-  }
-}
-.fish2 {
-  top: 70%;
-  right: 88%;
-  animation:fuwafuwa 3s infinite linear alternate;
-  animation-delay: 2.3s;
-  &:hover + .circle {
-    display: block;
-    top: 70%;
-    right: 88%;
-  }
-}
-.fish3 {
-  top: 9%;
-  right: 43%;
-  animation:fuwafuwa 3s infinite linear alternate;
-  animation-delay: 0.6s;
-  &:hover + .circle {
-    display: block;
-    top: 9%;
-    right: 43%;
-  }
-}
-.fish4 {
-  top: 50%;
-  right: 83%;
-  animation:fuwafuwa 3s infinite linear alternate;
-  animation-delay: 0.9s;
-  &:hover + .circle {
-    display: block;
-    top: 50%;
-    right: 83%;
-  }
-}
-.fish5 {
-  top: 60%;
-  right: 11%;
-  animation:fuwafuwa 3s infinite linear alternate;
-  animation-delay: 1.2s;
-  &:hover + .circle {
-    display: block;
-    top: 60%;
-    right: 11%;
-  }
-}
-.fish6 {
-  top: 86%;
-  right: 75%;
-  animation:fuwafuwa 3s infinite linear alternate;
-  animation-delay: 1.5s;
-  &:hover + .circle {
-    display: block;
-    top: 86%;
-    right: 75%;
-  }
-}
-.fish7 {
-  top: 6%;
-  right: 14%;
-  animation:fuwafuwa 3s infinite linear alternate;
-  animation-delay: 1.8s;
-  &:hover + .circle {
-    display: block;
-    top: 6%;
-    right: 14%;
-  }
-}
-.circle {
-  position: absolute;
-  width: 20px;
-  height: 20px;
-  border: solid thin #8fa2e4;
-  border-radius: 50%;
-  display: none;
-  animation: wave 1.6s infinite;
-}
-@keyframes wave {
-  0% {transform: scale(1);}
-  100% {transform: scale(4); opacity: 0;}
-}
-.fuwafuwa {
-  animation:fuwafuwa 3s infinite linear alternate;
-}
-@keyframes fuwafuwa {
-0% {transform:translate(0, 0) rotate(-10deg);}
-50% {transform:translate(0, -10px) rotate(0deg);}
-100% {transform:translate(0, 0)rotate(10deg);}
+  bottom: 20vh;
+  right: 68%;
+  color: #c498a7;
 }
 .showtitle {
   color: #fff;
